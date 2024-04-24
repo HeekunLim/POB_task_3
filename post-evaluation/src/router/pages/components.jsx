c406a270e3ae498969dca7b5c1cbf6a382b4a800
HeekunLim <fanghk555@gmail.com>
2024-04-24 19:09:01 +0900

    3주 형태로 변경

--- post-evaluation/src/router/pages/components.jsx
+++ post-evaluation/src/router/pages/components.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const RateBlock = ({ rate }) => {
   const dateObj = new Date(rate.date);
@@ -24,4 +25,58 @@ const RateBlock = ({ rate }) => {
   );
 };
 
-export { RateBlock };
+// db 내용 출력
+const NewCard = ({ rates }) => {
+  let navigate = useNavigate();
+
+  const handleClick = (checkid) => {
+    navigate("/detail/" + checkid);
+  };
+
+  return (
+    <div>
+      {rates.map((r) => (
+        <StyledRateBlockWrapper key={r.id}>
+          <div>
+            <div>
+              <p style={{ width: "300px" }}>{r.date}</p>
+            </div>
+            <div style={{ display: "flex", justifyContent: "flex-start" }}>
+              <RateBlock key={r.id} rate={r} />
+              <StyledStarRating>
+                {r.rate > 0 ? "★".repeat(r.rate) : "-"}
+              </StyledStarRating>
+            </div>
+          </div>
+          <div>
+            <StyledButton onClick={() => handleClick(r.id)}>수정</StyledButton>
+          </div>
+        </StyledRateBlockWrapper>
+      ))}
+    </div>
+  );
+};
+
+export { RateBlock, NewCard };
+
+const StyledRateBlockWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  border: solid;
+  padding: 20px;
+  margin: 10px;
+`;
+
+const StyledStarRating = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 250px;
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+`;
--- post-evaluation/src/router/pages/list.jsx
+++ post-evaluation/src/router/pages/list.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setRates } from "../../redux/slices/slice";
 import Axios from "axios";
-import { RateBlock } from "./components";
-import { useNavigate } from "react-router-dom";
+import { NewCard } from "./components";
 import styled from "styled-components";
 
 const List = () => {
   const { rates } = useSelector((state) => state.rates);
   const dispatch = useDispatch();
-  let navigate = useNavigate();
 
   // mock api에서 데이터 불러오기
   React.useEffect(() => {
@@ -26,37 +24,15 @@ const List = () => {
     }
   }, []);
 
-  // 버튼 클릭시 수정 페이지로 이동
-  const handleClick = (checkid) => {
-    navigate("/detail/" + checkid);
-  };
-
   return (
     <StyledContainer>
       <div>
         <h1>일주일 컨디션</h1>
       </div>
-      <div>
-        {rates.map((r) => (
-          <StyledRateBlockWrapper key={r.id}>
-            <div>
-              <div>
-                <p style={{ width: "300px" }}>{r.date}</p>
-              </div>
-              <div style={{ display: "flex", justifyContent: "flex-start" }}>
-                <RateBlock key={r.id} rate={r} />
-                <StyledStarRating>
-                  {r.rate > 0 ? "★".repeat(r.rate) : "-"}
-                </StyledStarRating>
-              </div>
-            </div>
-            <div>
-              <StyledButton onClick={() => handleClick(r.id)}>
-                수정
-              </StyledButton>
-            </div>
-          </StyledRateBlockWrapper>
-        ))}
+      <div style={{ display: "flex", justifyContent: "flex-start" }}>
+        <NewCard rates={rates.slice(0, 7)} />
+        <NewCard rates={rates.slice(7, 14)} />
+        <NewCard rates={rates.slice(14, 21)} />
       </div>
     </StyledContainer>
   );
@@ -69,26 +45,4 @@ const StyledContainer = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-`;
-
-const StyledRateBlockWrapper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  border: solid;
-  padding: 20px;
-  margin: 10px;
-`;
-
-const StyledStarRating = styled.p`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 250px;
-`;
-
-const StyledButton = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 20px;
 `;
